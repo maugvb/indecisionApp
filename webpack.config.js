c5722aea7efcf1dab74c907aa8b6417863185d1e
Mauricio <slashera4@gmail.com>
2019-02-02 14:50:42 +0100

    Config of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,109 +1,29 @@
-//entry -> output
-// var path = require('path');
-// var webpack = require('webpack');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-
-// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-
-// if (process.env.NODE_ENV === 'test') {
-//     require('dotenv').config({ path: '.env.test' });
-// } else if (process.env.NODE_ENV === 'development') {
-//     require('dotenv').config({ path: '.env.developmenT' });
-// }
-
-
-// module.exports = (env) => {
-//     const isProduction = env === 'production';
-//     const CSSExtract = new ExtractTextPlugin('styles.css');
-
-
-//     return {
-//     entry: './src/app.js',
-//     output: {
-//         path: path.join(__dirname, 'public'),
-//         filename: 'bundle.js'
-//     },
-//     watch: true,
-//     module: {
-//         rules: [{
-
-//             loader: 'babel-loader',
-//             test: /\.js$/,
-//             exclude: /node_module/
-//         }, {
-//             test: /\.s?css$/,
-//             use: [
-//                 {
-//                     loader: 'css-loader',
-//                     options: {
-//                       sourceMap: true
-//                     }
-//                   },
-//                 {
-//                     loader: 'sass-loader',
-//                     options: {
-//                       sourceMap: true
-//                     }
-//                 }
-//             ]
-//         }],
-//     },
-//     pugins: [
-//         CSSExtract
-//     ],
-//     devtool: isProduction ? 'source-map' : 'inline-source-map',
-//     devServer: {
-//         contentBase: path.join(__dirname, 'public'),
-//         historyApiFallback: true,
-//         publicPath: '/dist/'
-//     }
-// }};
-
-
-
 const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.developmenT' });
-}
-
-module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
-
-  return {
-    entry: ['babel-polyfill', './src/app.js'],
-    output: {
+module.exports = {
+  mode: 'development',
+  entry: ['babel-polyfill', './src/app.js'],
+  output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
-    },
-    module: {
-      rules: [{
-        loader: 'babel-loader',
+  },
+  module: {
+    rules: [
+      {
         test: /\.js$/,
+        loader: 'babel-loader',
         exclude: /node_modules/
-      }, {
+      },
+      {
         test: /\.s?css$/,
-        loader:"style-loader!css-loader!sass-loader"
-      }]
-    
+        use: ['style-loader', 'css-loader', 'sass-loader'] // Order seems important
+      }
+    ]
+  },
 
+  devtool: 'inline-source-map',
 
-
-    },
-    plugins: [
-    ],
-    devtool: /*isProduction ? 'source-map' : */'inline-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'public'),
-    }
-  };
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+  }
 };
